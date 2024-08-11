79b15689308a1acd7470bb51b5ed5350a6e74f87
Deekshith <42911939+deekshithhh@users.noreply.github.com>
2024-08-11 06:36:52 +0000

    Error Handling

--- xspellcheck/src/Components/Spellcheck.jsx
+++ xspellcheck/src/Components/Spellcheck.jsx
@@ -11,18 +11,17 @@ export default function Xspellcheck() {
     exampl: "example",
   };
   const handlechange = (e) => {
-    console.log(e.target.value);
-    setText(e.target.value);
-    const words = text.split(" ");
-    console.log(words, "Words");
+    const newtext = e.target.value;
+    setText(newtext);
+    // console.log(newtext, "New");
+    const words = newtext.split(" ");
+    // console.log(words, "Words");
     const correctedWords = words.map((word) => {
       const correctedWord = customDictionary[word.toLowerCase()];
-      console.log(correctedWord, word, "correctedword");
+      //   console.log(correctedWord, word, "correctedword");
       return correctedWord || word;
     });
     const correctedText = correctedWords.join(" ");
-
-    // Set the suggested text (first corrected word)
     const firstCorrection = correctedWords.find(
       (word, index) => word !== words[index],
     );
@@ -32,7 +31,11 @@ export default function Xspellcheck() {
   return (
     <div>
       <h1>Spell Check and Auto-Correction</h1>
-      <textarea placeholder="Enter text..." onChange={handlechange}></textarea>
+      <textarea
+        value={text}
+        placeholder="Enter text..."
+        onChange={handlechange}
+      ></textarea>
       {suggestedword && <div>Did you mean: {suggestedword}?</div>}
     </div>
   );
